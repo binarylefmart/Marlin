d453f8fbe987055b52817ff0a3de57d562cbb8ac
binarylefmart <lm.admin@binarylefmnart.online>
2022-04-14 19:09:04 +0200

    fix buffer

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -875,14 +875,14 @@
  * Use G29 repeatedly, adjusting the Z height at each point with movement commands
  * or (with LCD_BED_LEVELING) the LCD controller.
  */
-//#define PROBE_MANUALLY
+#define PROBE_MANUALLY
 //#define MANUAL_PROBE_START_Z 0.2
 
 /**
  * A Fix-Mounted Probe either doesn't deploy or needs manual deployment.
  *   (e.g., an inductive probe or a nozzle-based probe-switch.)
  */
-#define FIX_MOUNTED_PROBE
+//#define FIX_MOUNTED_PROBE
 
 /**
  * Use the nozzle as the probe, as with a conductive
